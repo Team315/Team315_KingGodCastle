30b893fcde6ed2cce2b21df5bcb600d410a181c5
상화\상화 <bdbd5066@naver.com>
2022-11-09 12:27:49 +0900

    CSV 작업중

--- Team315/main.cpp
+++ Team315/main.cpp
@@ -3,6 +3,7 @@
 
 int main()
 {
+    CLOG::WriteCSV("UpMove", 1, 10, 4, "Charactor/Monster/Chap1/Chap1-1Move", 80, 160, 80, 80, 40, 40, true);
     FRAMEWORK->Init(WINDOW_WIDTH, WINDOW_HEIGHT, "King God Castle");
     FRAMEWORK->Do();
 
