12af4cb2f2e1abb057b571fa6bdc0cdd5fa4a073
realstone513 <ting6247@nate.com>
2022-11-17 05:06:22 +0900

    edit constant.h

--- Team315/99.Include/Constant.h
+++ Team315/99.Include/Constant.h
@@ -1,30 +1,31 @@
 #pragma once
 
-// window size
+// Window size
 #define WINDOW_WIDTH (1280)
 #define WINDOW_HEIGHT (800)
 
-//MAX Count
-#define ChapterMaxCount (3)
-#define StageMaxCount (10)
+// MAX Count
+#define CHAPTER_MAX_COUNT (3)
+#define STAGE_MAX_COUNT (10)
 
-// tile
+// Tile
 #define TILE_SIZE (51.f)
+#define TILE_SIZE_HALF (25.f)
 #define TILE_WIDTH (10)
 #define TILE_HEIGHT (15)
 #define GAME_TILE_WIDTH (7)
 #define GAME_TILE_HEIGHT (14)
 
 
-#define Type1_Tile_Count (25)
-#define Type1_Obstacle_Count (3)
+#define TYPE1_TILE_COUNT (25)
+#define TYPE1_OBSTACLE_COUNT (3)
 
-// game screen size
+// Game screen size
 #define GAME_SCREEN_WIDTH (510.f)
 #define GAME_SCREEN_HEIGHT (765.f)
 #define GAME_SCREEN_ZOOM_WIDTH (450.f)
 #define GAME_SCREEN_ZOOM_HEIGHT (688.5f)
 
-// anony
+// No classification
 #define PREPARE_SIZE (14)
 #define PRESET_SIZE (6)
