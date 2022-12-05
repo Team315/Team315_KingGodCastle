8b82610cb7d6db01272047dbaf38209ceb81b18e
realstone513 <ting6247@nate.com>
2022-12-05 13:35:39 +0900

    slime00 임시 스킬 (범위 공격)

--- Team315/03.GameObject/Monster/Slime00.h
+++ Team315/03.GameObject/Monster/Slime00.h
@@ -4,11 +4,15 @@
 class Slime00 : public Character
 {
 protected:
+	float mpTimer;
+	float duration;
+
 public:
 	Slime00(int skillTier = 0);
 	virtual ~Slime00();
 
 	virtual void Init() override;
+	virtual void Reset() override;
 	virtual void Update(float dt) override;
 	virtual void Draw(RenderWindow& window) override;
 	virtual void SetPos(const Vector2f& pos) override;
@@ -20,4 +24,3 @@ public:
 	void UpdateIdle(float dt);
 	void UpdateSkill(float dt);
 };
-
--- Team315/03.GameObject/Skill/Slime00Skill.h
+++ Team315/03.GameObject/Skill/Slime00Skill.h
@@ -3,5 +3,12 @@
 
 class Slime00Skill : public Skill
 {
-};
+protected:
 
+public:
+	Slime00Skill(int starNumber);
+	virtual ~Slime00Skill();
+
+	virtual void SetSkillRange(Vector2f startPos) override;
+	virtual void CastSkill(Character* caster) override;
+};
